439d52451af38826cf35bca6817f02fac72c633e
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-07-07 18:54:38 -0700

    Initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,35 @@
+document.onload = dataFlow()
+
+async function fetchData(){
+    let response= await fetch('./assets/json/data.json')
+    let data= await response.json()
+    return data;
+}
+
+function showJobs(data){
+    let jobContainer= document.querySelector('.job-tile-container')
+    jobHTML= ""
+    data.forEach(function (job){
+        jobHTML += `
+        <div class="job-tile">
+                <div class="job-tile-head">
+                    <div class="logo"><img src="${job.logo}"/></div>
+                    <div class="dots"><i class="fas fa-ellipsis-h"></i></div>
+                </div>
+                <div class="job-tile-title">${job.roleName}</div>
+                <div class="job-tile-desc">${job.requirements.content}</div>
+                <div class="btns">
+                    <div class="apply">Apply</div>
+                    <div class="message">Message</div>
+                </div>
+            </div>
+        `
+    });
+    jobContainer.innerHTML = jobHTML
+}
+
+async function dataFlow(){
+    const data= await fetchData();
+    showJobs(data);
+}
+ 
