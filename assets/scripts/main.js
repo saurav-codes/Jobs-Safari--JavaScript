331ab8110e77d3de77591c9c819df88aaf6c699a
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-07-09 18:29:05 -0700

    added search functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,4 @@
-console.log('working');
-document.onload = dataFlow()
+document.onload = loadInitial_data()
 
 async function fetchData(){
     let response= await fetch('./assets/json/data.json')
@@ -29,8 +28,49 @@ function showJobs(data){
     jobContainer.innerHTML = jobHTML
 }
 
-async function dataFlow(){
+async function loadInitial_data(){
     const data= await fetchData();
     showJobs(data);
 }
- 
+// ===================================================
+// ======== SEARCH FUNCTIONALITY =================
+// --------------------------------------------------
+let inpField_el= document.getElementById('filter-jobs') 
+
+let find_btn= document.querySelector('.btn-container')
+find_btn.addEventListener('click',Search_main);
+
+async function searchDB(searchTxt, Jobs){ 
+    let searched_Job= []
+    for (let job of Jobs){
+        let tmpJob= await searchJob(job,searchTxt) 
+        console.log('job found is:',tmpJob);
+        if (tmpJob){
+            searched_Job.push(tmpJob)
+        }        
+    }
+    if (searched_Job){
+        return searched_Job;
+    }else{
+        return;
+    }
+}
+async function searchJob(job,searchTxt){
+    // logic for searching job
+    console.log('searchJob: user asked for',searchTxt);
+    if ((job.roleName.toLowerCase().includes(searchTxt))||(job.requirements.content.toLowerCase().includes(searchTxt))){
+        console.log('searchJob: found this=> ',job);
+        return job;
+    }else{
+        return;
+    }
+}
+async function Search_main(){
+    if (inpField_el.value){
+        let all_jobs= await fetchData()
+        let filtered_jobs= await searchDB(inpField_el.value, all_jobs)
+        console.log(filtered_jobs);
+        showJobs(filtered_jobs)
+        console.log('jobs shown successfully');
+    }
+}
